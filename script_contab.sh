64499ff0b254d0cd52f3c0966bf55f490023ba3d
RiadElamine <RiadElamineDevInfo@gmail.com>
2024-03-22 15:23:54 +0000

    Update script_contab.sh

--- script_contab.sh
+++ script_contab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minute=`who | awk '{print $4}' | awk -F ':' '{print $1}' | awk 'NR == 1'`
-s=`who | awk -F ':' '{print $2}' | awk 'NR == 1'`
+minute=`who | awk 'NR == 1 {print $4}' | awk -F ':' '{print $1}'`
+s=`who | awk -F ':' 'NR == 1 {print $2}'`
 second=`echo $minute $s | awk '{printf("%d", $1 % 10 * 60 + $2)}'`
 sleep $second
